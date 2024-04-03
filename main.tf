8a45fc77ab86f7c3c2595355e5f84e92bb3a1f83
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-03 22:42:00 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_s3_bucket_object_lock_configuration" "tf_backend_bucket_object_loc
 }
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "tf_backend_bucket_sse" {
-  bucket = aws_s3_bucket.tf_backend_bucket.id
+  bucket = aws_s3_bucket.bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"

ccf453da0185fa7e65d3724e48a27feaf673d536
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-03 23:19:35 +0545

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 variable "bucket_name" {
   type        = string
-  default     = "terraform_with_gitops_pri_deploy_s3_frontend_v1.0.0"
+  default     = "terraformwithgitopsprideploys3frontend1.0.0"
   description = "bucket name for gitops"
 }
 variable "default_tags" {

70008f811405a4556a6e163ec996286a6eabee59
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-03 22:42:08 +0545

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,3 +8,13 @@ variable "bucket_name" {
   default     = "terraform_with_gitops_pri_deploy_s3_frontend_v1.0.0"
   description = "bucket name for gitops"
 }
+variable "default_tags" {
+  description = "Default tags to apply to resources"
+  type        = map(string)
+  default     = {
+    Name     = "Priyanka Gitops assignment tags"
+    Creator  = "priyankatuladharmail@gmail.com"
+    Deletable = "Yes"
+    Project  = "Intern"
+  }
+}

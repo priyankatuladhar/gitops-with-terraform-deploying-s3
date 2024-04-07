4d163ad2422efef1e900c7710c9192369c529a6b
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-07 18:25:42 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
 provider "aws" {
   region = var.region
-}
 
+  //tags I can use anywhere in this file
+  default_tags {
+    tags = {
+      Name      = "Priyanka Gitops assignment tags"
+      Creator   = "priyankatuladharmail@gmail.com"
+      Deletable = "Yes"
+      Project   = "Intern"
+    }
+  }
+}
 terraform {
   backend "s3" {
-    bucket         = "priyanka-gitops-ops-frontend-bucket"
+    bucket         = "terraformwithgitopsprideploys3frontend"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "pri-tf-lockfile"
@@ -12,22 +21,29 @@ terraform {
 }
 
 locals {
-  bucket_name = "${var.s3_dist_bucket}-${terraform.workspace}"
+  bucket_name_new = "${var.bucket_name}-${terraform.workspace}"
 }
 
-resource "aws_s3_bucket_versioning" "frontend_dist_versioning" {
-  bucket = aws_s3_bucket.frontend_dist.id
+# path = "./statefile.tfstate"
+
+
+resource "aws_s3_bucket" "tf_backend_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_versioning" "tf_backend_bucket_versioning" {
+  bucket = aws_s3_bucket.tf_backend_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
-  
-resource "aws_s3_bucket_server_side_encryption_configuration" "frontend_dist_encryption" {
-  bucket = aws_s3_bucket.frontend_dist.id
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "tf_backend_bucket_sse" {
+  bucket = aws_s3_bucket.tf_backend_bucket.id
   rule {
     apply_server_side_encryption_by_default {
       sse_algorithm = "aws:kms"
     }
   }
 }
- 

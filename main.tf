baa8ac1c53b3513f86957d7ee38d7bac2261f320
Priyanka Tuladhar <136885106+priyankatuladhar@users.noreply.github.com>
2024-04-07 18:01:42 +0545

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
   
   terraform {
 	backend "s3" {
-	  bucket         = "terraformwithgitopsprideploys3frontendpriyanka"
+	  bucket         = "terraformwithgitopsprideploys3frontendpriyanka1.0"
 	  key            = "terraform.tfstate"
 	  region         = "us-east-1"
 	  dynamodb_table = "terraform_state_pri"
